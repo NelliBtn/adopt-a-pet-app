9476f4619e04ce5e550c43d5303c6a4bff1d66f3
Nelli Butenko <butenkonelly93@gmail.com>
2021-07-25 14:12:29 -0400

    add search page route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
       <Navigation />
 
       <Switch>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
         <Route path="/:type/:id">
           <PetDetailsPage />
         </Route>
